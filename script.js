15317286cc04fc89b771c465208f4110663b2bf9
Patrick Dunn <pmdunn78@gmail.com>
2021-01-15 00:29:37 -0600

    copy to clipboard functionality

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var copyBtn =document.querySelector('#copy');
+
+function copyToClipboard() {
+  /* Get the text field */
+  var copyText = document.getElementById("password");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Password copied: " + copyText.value);
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -8,7 +24,12 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+copyBtn.addEventListener("click", copyToClipboard);
+
+

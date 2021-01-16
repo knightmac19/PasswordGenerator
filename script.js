b487c7749f416a08c42511ce77a6db319144c175
Patrick Dunn <pmdunn78@gmail.com>
2021-01-15 19:35:11 -0600

    done: only show btn's after checkbox selected

--- script.js
+++ script.js
@@ -1,78 +1,80 @@
-// grabbing copy and generate button
+// grabbing elements from the DOM
 var generateBtn = document.querySelector("#generate");
 var copyBtn = document.querySelector('#copy');
-
-generateBtn.classList.add('invisible');
-copyBtn.classList.add('invisible');
-
 var specialCheck = document.querySelector('#special');
 var numbersCheck = document.querySelector('#numbers');
-var lengthInput = document.querySelector('#length');
 var lowerCheck = document.querySelector('#lowercase');
 var upperCheck = document.querySelector('#uppercase');
+var lengthInput = document.querySelector('#length');
 
 // boolean status checkers
-let generated = false;
-let specialChars = false;
-let numbers = false;
+var generated = false;
+var specialChars = false;
+var numbers = false;
 let lowercase = false;
-let uppercase = false;
-let lengthBoolean = false;
-let length = 0;
+var uppercase = false;
 
+// initialize length & password
+var length = 0;
+var password = '';
+
+// event listeners for checkboxes & length input
 specialCheck.addEventListener('change', function() {
   if (this.checked) {
-    console.log("special is checked..");
+    specialChars = true;
     showGenerateBtn();
   } else {
-    console.log("special is not checked..");
+    return;
   }
 });
 
 numbersCheck.addEventListener('change', function() {
   if (this.checked) {
-    console.log("numbers is checked..");
+    numbers = true;
     showGenerateBtn();
   } else {
-    console.log("numbers is not checked..");
+    return;
   }
 });
 
 lowerCheck.addEventListener('change', function() {
   if (this.checked) {
-    console.log("lower is checked..");
+    lowercase = true;
     showGenerateBtn();
   } else {
-    console.log("lower is not checked..");
+    return;
   }
 });
 
 upperCheck.addEventListener('change', function() {
   if (this.checked) {
-    console.log("upper is checked..");
+    uppercase = true;
+    
     showGenerateBtn();
   } else {
-    console.log("upper is not checked..");
+    return;
   }
 });
 
 lengthInput.addEventListener('change', function() {
   if (this.value >= 8 && this.value <129) {
     length = this.value;
-    console.log("lengthInput is " + length);
     showGenerateBtn();
   } else {
-    console.log("invalid length Input");
+    return;
   }
 });
 
 function showGenerateBtn() {
-  if (length >=8 && length < 129) {
-    generateBtn.classList.remove('invisible');
-    generateBtn.classList.add('visible');
-  } else {
-    return;
-  }
+  if (length != 0) {
+    if (specialChars || numbers || lowercase || uppercase) {
+      generateBtn.classList.remove('invisible');
+      generateBtn.classList.add('visible');
+    } else {
+      return;
+    }
+  } 
+  return;
 };
 
 function showCopyBtn() {
@@ -80,43 +82,32 @@ function showCopyBtn() {
   copyBtn.classList.add('visible');
 };
 
-
-// if (specialChars || numbers || lowercase || uppercase && lengthBoolean) {
-//   generateBtn.classList.remove('invisible');
-//   generateBtn.classList.add('visible');
-// }
-
-function myFunction() {
-  var element = document.getElementById("myDIV");
-  element.classList.add("mystyle");
-}
-
-
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  password = passwordText.value;
+  
+  // lastly, make copy button visible
+  showCopyBtn();
 
 };
 
+// copy functionality
 function copyToClipboard() {
-  /* Get the text field */
   var copyText = document.getElementById("password");
-
-  /* Select the text field */
+  
   copyText.select();
   copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-  /* Copy the text inside the text field */
+  
   document.execCommand("copy");
 
-  /* Alert the copied text */
   alert("Password copied: " + copyText.value);
 };
 
-// Add event listener to generate button
+// Add event listeners to generate & copy buttons
 generateBtn.addEventListener("click", writePassword);
 copyBtn.addEventListener("click", copyToClipboard);
 
